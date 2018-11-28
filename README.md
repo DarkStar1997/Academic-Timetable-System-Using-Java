e80380c12cd2babb8e74750e7c02eebb14006911
Rohan Mark Gomes <rohanmarkgomes@gmail.com>
2018-11-28 21:01:27 +0530

    Added build.sh

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # Academic-Timetable-System-Using-Java
 
 ## To Run The Program : 
+  cd src
+  chmod +x build.sh
+  ./build.sh
   java ATSManager
---
+++ src/build.sh
@@ -0,0 +1,14 @@
+rm *.class
+javac Courses.java
+javac Instructors.java
+javac LectureStack.java
+javac Period.java
+javac Periods.java
+javac Rooms.java
+javac SC.java
+javac SCIR.java
+javac Sections.java
+javac Timeslots.java
+javac DataManager.java
+javac LogicMgr.java
+javac ATSManager.java
